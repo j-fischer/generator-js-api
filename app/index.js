a59646b0eee68e600da9a0eb803353afc4e31f06
Johannes Fischer (MBP) <fischer.jh@gmail.com>
2016-02-19 21:52:27 -0700

    Upgraded generator project and deprecated modules calls.

--- app/index.js
+++ app/index.js
@@ -3,15 +3,15 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
-  
+module.exports = yeoman.Base.extend({
+
   _copy: function(src, dest) {
     this.fs.copy(
       this.templatePath(src),
       this.destinationPath(dest)
     );
   },
-  
+
   _copyAndReplace: function(src, dest, context) {
     this.fs.copyTpl(
       this.templatePath(src),
@@ -19,7 +19,7 @@ module.exports = yeoman.generators.Base.extend({
       context
     );
   },
-  
+
   initializing: function () {
     this.pkg = require('../package.json');
   },
@@ -38,13 +38,13 @@ module.exports = yeoman.generators.Base.extend({
       name    : 'namespace',
       message : 'What is the fully qualified name of your API (namepsace + class name), i.e. "com.mycompany.SomeApi"',
       default : "my.Api"
-    }, 
+    },
     {
       type    : 'input',
       name    : 'module',
       message : 'What is the name of your API\'s AMD module, i.e "some-api"',
       default : moduleName
-    }, 
+    },
     {
       type    : 'input',
       name    : 'filename',
@@ -60,48 +60,48 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
     app: function () {
       var answers = this.env.options;
-          
+
       this._copy('Gruntfile.js', 'Gruntfile.js');
-      
+
       this._copyAndReplace(
         'package.json',
-        'package.json', 
+        'package.json',
         {
-          packageName: answers.filename 
+          packageName: answers.filename
         }
       );
-      
+
       this._copyAndReplace(
-        'src/main/javascript/api.js', 
-        'src/main/javascript/' + answers.filename + '.js', 
+        'src/main/javascript/api.js',
+        'src/main/javascript/' + answers.filename + '.js',
         {
           apiName: answers.namespace,
           moduleName: answers.module.toLowerCase()
         }
       );
-      
+
       this._copyAndReplace(
-        'src/test/javascript/api.spec.js', 
-        'src/test/javascript/' + answers.filename + '.spec.js', 
+        'src/test/javascript/api.spec.js',
+        'src/test/javascript/' + answers.filename + '.spec.js',
         {
           apiName: answers.namespace
         }
       );
-      
+
       this._copy('src/test/karma.conf.js', 'src/test/karma.conf.js');
     },
 
     projectfiles: function () {
       var answers = this.env.options;
-      
+
       this._copy('editorconfig', '.editorconfig');
       this._copy('gitignore', '.gitignore');
       this._copy('hgignore', '.hgignore');
-      
+
       this._copyAndReplace(
-        'jshintrc', 
-        '.jshintrc', 
-        { 
+        'jshintrc',
+        '.jshintrc',
+        {
           namespaceRoot: answers.namespace.split('.')[0]
         }
       );
@@ -109,7 +109,7 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   end: function () {
-    var generator = this;    
+    var generator = this;
     generator.installDependencies({
       skipInstall: generator.options['skip-install'],
       bower: false,
@@ -118,7 +118,7 @@ module.exports = yeoman.generators.Base.extend({
           generator.log("All done. The installation of the dependencies has been skipped. You need to run 'npm install' before you can run 'grunt' to build your API. If karma cannot find PhantomJS, run 'npm install karma-phantomjs-launcher' to install its missing dependencies. Cheers");
         } else {
           /* The following installation will install the missing dependencies for the karma-phantomjs-launcher. Those dependencies won't be installed
-           * when running 'npm install' as this only installs top-level modules listed in package.json (see https://github.com/npm/npm/issues/1341). 
+           * when running 'npm install' as this only installs top-level modules listed in package.json (see https://github.com/npm/npm/issues/1341).
            */
           generator.log("Running 'npm install karma-phantomjs-launcher' to ensure its dependencies will be installed. This will avoid issues with PhantomJS when running grunt.");
           generator.npmInstall(['karma-phantomjs-launcher']);
--- test/test-app.js
+++ test/test-app.js
@@ -5,8 +5,8 @@ var path = require('path');
 var fs = require('fs-extra');
 var exec = require('child_process').exec;
 var expect = require ('chai').expect;
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
 var os = require('os');
 
 describe('js-api:app with skip-install option', function () {
@@ -28,7 +28,7 @@ describe('js-api:app with skip-install option', function () {
       'Gruntfile.js'
     ]);
   });
-  
+
   it('creates default api and test files', function () {
     assert.file([
       'src/main/javascript/api.js',
@@ -36,7 +36,7 @@ describe('js-api:app with skip-install option', function () {
       'src/test/karma.conf.js'
     ]);
   });
-  
+
   it('does not install node modules ', function () {
     assert.noFile([
       'node_modules'
@@ -45,7 +45,7 @@ describe('js-api:app with skip-install option', function () {
 });
 
 describe('js-api:app with user overrides', function () {
-  
+
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
@@ -53,13 +53,13 @@ describe('js-api:app with user overrides', function () {
       .withPrompts({ namespace: 'some.Api', module: 'some-api', filename: 'someApi' })
       .on('end', done);
   });
-  
+
   it('creates user specified api and test files', function () {
     assert.file([
       'src/main/javascript/someApi.js',
       'src/test/javascript/someApi.spec.js'
     ]);
-    
+
     assert.fileContent('src/main/javascript/someApi.js', /window\.some\.Api = API\;/);
     assert.fileContent('src/main/javascript/someApi.js', /define\(\"some-api\"\,/);
     assert.fileContent('.jshintrc', /"some"\: true/);
@@ -67,27 +67,27 @@ describe('js-api:app with user overrides', function () {
 });
 
 describe('run grunt', function () {
-  // Build should pass in less than 60s even on older computers. 
+  // Build should pass in less than 60s even on older computers.
   // Tested on late 2011 Mac Book Pro with 30-60s execution time.
   this.timeout(120000);
-  
+
   var app;
-  
+
   before(function (done) {
     this.timeout(4000);
-    
+
     app = helpers
       .run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'), function (dir) {
-        
+
         // Note: Assumes that 'npm install' was run inside the fixtures folder.
         fs.symlinkSync(path.join(__dirname, 'fixtures/node_modules'),
           path.join(dir, 'node_modules'), 'dir');
-        
+
         done();
       });
   });
-  
+
   it ('should pass grunt build', function (done) {
     app
       .withOptions({ 'skip-install': true })
@@ -96,14 +96,14 @@ describe('run grunt', function () {
         exec('grunt', function (error, stdout, stderr) {
           if (error)
             console.log('Error: ' + error);
-          
-          expect(stdout).to.contain('No problems');  
+
+          expect(stdout).to.contain('No problems');
           expect(stdout).to.contain('Executed 1 of 1 SUCCESS');
           expect(stdout).to.contain('Done, without errors.');
-          
+
           assert.file(['docs/coverage', 'docs/jsdoc']);
           done();
         });
       });
-    });      
+    });
   });
